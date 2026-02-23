6df05b9d5e3be3283bf1612fafb164f39f9e3b05
thulasehaesan29 <Thulasehaesan@gmail.com>
2026-02-23 09:22:08 +0530

    I updated the pharmacy register

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 
 const authRoutes = require("./routes/authRoutes");
 const patientRoutes = require("./routes/patientRoutes");
+const pharmacyRoutes = require("./routes/pharmacyRoutes");
 
 const app = express();
 
@@ -12,6 +13,7 @@ app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/patients", patientRoutes); 
+app.use("/api/pharmacies", pharmacyRoutes);
 
 const PORT = 5000;
 app.listen(PORT, () => console.log("Server running on port " + PORT));
